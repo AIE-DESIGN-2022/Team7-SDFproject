0c1408bbc8754730e7993d36ba28b81f008dc10e
AIE\s200552 <s200552@students.aie.edu.au>
2022-07-06 13:44:22 +0930

    Day 3: added leantween, made dialogue box, starting animating UI

---
+++ Team7SDF/Assets/NotificationTween.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class NotificationTween : MonoBehaviour
+{
+    public UnityEvent onCompleteCallBack;
+
+    public float startingX;
+    public float startingY;
+
+    public float distanceX;
+    public float durationX;
+    public float distanceY;
+    public float durationY;
+    public float delay;
+    public float lingerDelay;
+    public float reset;
+    public LeanTweenType easeType;
+
+    void Start()
+    {
+        //LeanTween.scale(gameObject, new Vector3(0, 0, 0), 0.5f).setOnComplete(DestroyMe);
+        LeanTween.moveY(gameObject, distanceY, durationY).setDelay(delay).setEase(easeType).setOnComplete(FadeOut);
+        LeanTween.moveX(gameObject, distanceX, durationX).setDelay(delay).setEase(easeType).setOnComplete(OnComplete);
+    }
+
+    public void OnComplete()
+    {
+        if (onCompleteCallBack != null)
+        {
+            onCompleteCallBack.Invoke();
+        }
+    }
+
+    public void FadeOut()
+    {
+        LeanTween.moveX(gameObject, startingX, reset).setDelay(lingerDelay).setOnComplete(ResetPosition);
+    }
+
+    public void ResetPosition()
+    {
+        LeanTween.moveY(gameObject, startingY, durationY).setDelay(lingerDelay);        
+    }
+}
---
+++ Team7SDF/Assets/Scripts/UI/ResourceCostUpdater.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class ResourceCostUpdater : MonoBehaviour
+{
+    public LayoutElement layoutElement;
+    public int resourceCount;
+    public GameObject costObjectOne;
+    public GameObject costObjectTwo;
+    public GameObject costObjectThree;
+
+    private void Update()
+    {
+        if (costObjectThree.activeInHierarchy)
+        {
+            resourceCount = 3;
+        }
+        else if(costObjectTwo.activeInHierarchy)
+        {
+            resourceCount = 2;
+        }
+        else if(costObjectOne.activeInHierarchy)
+        {
+            resourceCount = 1;
+        }
+
+        if (resourceCount == 1)
+        {
+            layoutElement.minWidth = 0;
+        }
+        else if (resourceCount == 2)
+        {
+            layoutElement.minWidth = 1.81f;
+        }
+        else if (resourceCount == 3)
+        {
+            layoutElement.minWidth = 2.65f;
+        }
+    }
+}
