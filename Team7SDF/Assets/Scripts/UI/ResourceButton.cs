a812f22c511d8eb8e02b13c306d4fa542c2d149a
AIE\s200552 <s200552@students.aie.edu.au>
2022-07-20 16:09:01 +0930

    added shop functions :)

---
+++ Team7SDF/Assets/Scripts/UI/ResourceButton.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ResourceButton : MonoBehaviour
+{
+    public ResourceType resourceType;
+    public int quantity;
+    public int price;
+    public int sellPrice;
+    public TextMeshProUGUI priceText;
+    public TextMeshProUGUI sellPriceText;
+
+    private void Start()
+    {
+        if (price == 0 && priceText == null)
+        {
+            SetSellPrice();
+        }
+        else if (sellPrice == 0 && sellPriceText == null)
+        {
+            SetBuyPrice();
+        }
+    }
+    public void SetBuyPrice()
+    {
+        priceText.text = price.ToString();
+    }
+    public void SetSellPrice()
+    {
+        sellPriceText.text = sellPrice.ToString();
+    }
+}
